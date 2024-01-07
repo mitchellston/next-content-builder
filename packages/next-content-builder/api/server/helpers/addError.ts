9e3e2f46a9a9dcf7eedf1fc04948c865386ff572
mitchellston <m.steenwijk@live.nl>
2024-01-07 01:16:30 +0100

    addError type error fix

--- packages/next-content-builder/api/server/helpers/addError.ts
+++ packages/next-content-builder/api/server/helpers/addError.ts
@@ -1,6 +1,12 @@
-export const addError = (errors: object, key: string, error: string) => {
-  if (key in errors && Array.isArray(errors[key]))
-    errors[key].push(error ?? "This input can not be used multiple times");
-  else errors[key] = [error];
+export const addError = (
+  errors: { [key: string]: unknown },
+  key: string,
+  error: string
+) => {
+  if (key in errors) {
+    const val = Array.isArray(errors[key]);
+    if (Array.isArray(val))
+      val.push(error ?? "This input can not be used multiple times");
+  } else errors[key] = [error];
   return errors;
 };
